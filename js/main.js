02f83ad470b5012b2edcca95e664ca561261601f
Devin Wieland <devinwieland@gmail.com>
2014-01-24 13:47:06 -0500

    v1 complete

--- js/main.js
+++ js/main.js
@@ -1,1 +1,5 @@
-
+$(function() {                       //run when the DOM is ready
+  $("#button").click(function() {  //use a class, since your ID gets mangled
+    $("#wufoo-r84f9i0942lt3").addClass("highlight");      //add the class to the clicked element
+  });
+});
